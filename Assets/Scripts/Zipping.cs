31abde7828cf95c03a5e0914eabdfdb3518e0ff8
Lewis <135326467+Paite-ACM@users.noreply.github.com>
2023-07-19 18:54:53 +0100

    fuck
    forgot to save

--- Assets/Scripts/Zipping.cs
+++ Assets/Scripts/Zipping.cs
@@ -4,8 +4,13 @@ using UnityEngine;
 
 public class Zipping : MonoBehaviour
 {
+    // important references 
     [SerializeField] private PlayerMovement plrMovement;
     [SerializeField] private Transform cam;
     [SerializeField] private Transform zipAimer;
     [SerializeField] private LayerMask zippable;
+
+    // zip stuff
+    [SerializeField] private float maxZipDistance;
+
 }
